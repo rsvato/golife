f63e4a15e010e08a6d679075dda7a0c7ea42e70b
Sviatoslav Reentenko <rsvato@gmail.com>
2026-02-02 01:14:40 +0300

    Use flat array

--- life.go
+++ life.go
@@ -2,32 +2,41 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 	"strings"
 )
 
 const (
-	width  = 80
-	height = 40
+	rows              = 10
+	cols              = 10
+	minNeighborsAlive = 2
 )
 
-type Field [][]bool
-
-func newField() Field {
-	var f = make(Field, height)
-	for i := range f {
-		f[i] = make([]bool, width)
+func round(coord int, around int) int {
+	for coord < 0 {
+		coord += around
 	}
+	if coord >= around {
+		coord = coord % around
+	}
+	return coord
+}
+
+type Field []bool
+
+func NewField() Field {
+	var f = make(Field, cols*rows)
 	return f
 }
 
-func (f Field) Show() string {
+func (f Field) String() string {
 	var builder strings.Builder
-	for i := range f {
-		for j := range f[i] {
-			if f[i][j] {
-				builder.WriteString("x")
+	for i := range cols {
+		for j := range rows {
+			if f[i*cols+j] {
+				builder.WriteString("█ ")
 			} else {
-				builder.WriteString(".")
+				builder.WriteString("  ")
 			}
 		}
 		builder.WriteString("\n")
@@ -35,7 +44,23 @@ func (f Field) Show() string {
 	return builder.String()
 }
 
+func (f Field) Seed() {
+	for i := range f {
+		x := rand.IntN(100)
+		f[i] = x%4 == 0
+	}
+}
+
+func (f Field) Alive(x int, y int) bool {
+	return f[x*rows+y]
+}
+
+func (f Field) AliveNeighbors(x int, y int) int {
+	return 0
+}
+
 func main() {
-	f := newField()
-	fmt.Println(f.Show())
+	f := NewField()
+	f.Seed()
+	fmt.Println(f)
 }
